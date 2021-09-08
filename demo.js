10451c2381ee730a86653a9e0254ea2291e0fc7c
Timothy Foster <tf.auroratide@gmail.com>
2021-09-08 12:55:04 -0500

    A few other demos

--- demo.js
+++ demo.js
@@ -1,5 +1,17 @@
-document.querySelector('#main-demo .start').onclick = () => {
-    document.querySelectorAll('#main-demo typewritten-text').forEach(elem => {
-        elem.start()
+const setupRun = (name) => {
+    document.querySelector(`#${name} .start`).onclick = () => {
+        document.querySelectorAll(`#${name} typewritten-text`).forEach(elem => {
+            elem.start()
+        })
+    }
+}
+
+setupRun('main-demo')
+setupRun('markdown-demo')
+setupRun('interval-demo')
+
+document.querySelector('#repeat-demo .toggle').onclick = () => {
+    document.querySelectorAll(`#repeat-demo typewritten-text`).forEach(elem => {
+        elem.paused = !elem.paused
     })
 }
